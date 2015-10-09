962af8a031b7e3bda76dc7c30276ad7d32e01fbf
Tom LaMantia <tom.lamantia@mail.utoronto.ca>
2015-10-09 11:02:56 -0400

    Initial file structure setup.

---
+++ Src/Main.py
@@ -0,0 +1,14 @@
+"""
+-------------------------------------------------------
+PySHARDS Cache Curve Modeling
+
+This is a Python implementation of SHARDS, an extension
+of Mattson's MRC construction formulated by Waldspurger et al.
+in their 2015 paper "Efficient MRC Construction with SHARDS".
+-------------------------------------------------------
+Author:  Tom LaMantia
+ID:      your ID
+Email:   tom.lamantia@mail.utoronto.ca
+Version: October 9, 2015
+-------------------------------------------------------
+"""
